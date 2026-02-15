98d5f41cfffacc453f30522bc85604cfa1e0d557
bara-crypto <balac6796@gmail.com>
2026-02-15 15:01:21 +0530

    Add Calculator class with add and subtract methods

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+package com.example.web;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
